710fe61d12afdb570340996b5cea3cd78d501756
Yegor Kryukov <yegor.kryukov@gmail.com>
2018-04-03 22:26:38 -0400

    Update heroku vars reference

--- main.py
+++ main.py
@@ -1,62 +1,64 @@
 
 # coding: utf-8
 
-import os
-is_prod = os.environ.get('IS_HEROKU', None)
-
-if is_prod:
-
-
-    # Dependencies
-    import tweepy
-    import time
-    import json
-    import random
-    import requests as req
-    import datetime
-
-    #commenting this out because heroku vars should pick up
-    #from config import *
-
-    # Twitter API Keys
-    consumer_key = consumer_key
-    consumer_secret = consumer_secret
-    access_token = access_token
-    access_token_secret = access_token_secret
-    # Weather API Key
-    api_key = weather_api_key
-
-    def WeatherTweet():
-
-        # Construct a Query URL for the OpenWeatherMap
-        url = "http://api.openweathermap.org/data/2.5/weather?"
-        city = "Moscow"
-        units = "metric"
-        query_url = url + "appid=" + api_key + "&q=" + city + "&units=" + units
-
-        # Perform the API call to get the weather
-        r = req.get(query_url)
-        print(f'   requested URL: {r.url}')
-        
-        if r.status_code == 200:
-            response = r.json()
-            temp = response['main']['temp']
-        # Twitter credentials
-            auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-            auth.set_access_token(access_token, access_token_secret)
-            api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
-
-        # Tweet the weather
-            try:
-                api.update_status(f'The weather in {city} is {temp}°C now')
-                print('Just tweeted')
-            except tweepy.TweepError as e:
-                print(e)
-                pass      
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['consumer_key'], \
+                  os.environ['consumer_secret'], \
+                  os.environ['access_token'], \
+                  os.environ['access_token_secret'], \
+                  os.environ['weather_api_key'])
 
 
-    # Infinite loop
-    while True:
-        WeatherTweet()
-        time.sleep(3600)
+# Dependencies
+import tweepy
+import time
+import json
+import random
+import requests as req
+import datetime
+
+#commenting this out because heroku vars should pick up
+#from config import *
+
+# Twitter API Keys
+#consumer_key = consumer_key
+#consumer_secret = consumer_secret
+#access_token = access_token
+#access_token_secret = access_token_secret
+# Weather API Key
+#api_key = weather_api_key
+
+def WeatherTweet():
+
+    # Construct a Query URL for the OpenWeatherMap
+    url = "http://api.openweathermap.org/data/2.5/weather?"
+    city = "Moscow"
+    units = "metric"
+    query_url = url + "appid=" + api_key + "&q=" + city + "&units=" + units
+
+    # Perform the API call to get the weather
+    r = req.get(query_url)
+    print(f'   requested URL: {r.url}')
+    
+    if r.status_code == 200:
+        response = r.json()
+        temp = response['main']['temp']
+    # Twitter credentials
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+    # Tweet the weather
+        try:
+            api.update_status(f'The weather in {city} is {temp}°C now')
+            print('Just tweeted')
+        except tweepy.TweepError as e:
+            print(e)
+            pass      
+
+
+# Infinite loop
+while True:
+    WeatherTweet()
+    time.sleep(3600)
 
